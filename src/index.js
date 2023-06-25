3526e224e814ef2e4a9f1e59f714c6ee3abac413
michgund <117133781+michgund@users.noreply.github.com>
2023-06-25 10:58:33 +0900

    webpack setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+import PrintMe from "./print";
 import "./style.css";
+import Water from "./water.jpeg";
 
 function component() {
-  let newDiv = document.createElement("div");
+  const newDiv = document.createElement("div");
+  const btn = document.createElement("button");
+
   newDiv.innerHTML = "Hello newDiv!";
   newDiv.classList.add("red");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.addEventListener("click", PrintMe);
+
+  newDiv.appendChild(btn);
+
+  // Add the image
+  const myImage = new Image();
+  myImage.src = Water;
+
+  //   newDiv.appendChild(myImage);
 
   return newDiv;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Odin's Restaurant",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
